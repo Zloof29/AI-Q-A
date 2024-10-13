409435be04d5e780626ffa1d16091ca665657185
Lior Bas <look551@gmail.com>
2024-10-13 14:57:23 +0300

    fixed css and finished form

--- src/Components/LayoutArea/Main/Main.tsx
+++ src/Components/LayoutArea/Main/Main.tsx
@@ -1,9 +1,10 @@
-import "./Main.css";
+import { Input } from "../../InterviewArea/Input/Input";
+import css from "./Main.module.css";
 
 export function Main(): JSX.Element {
-    return (
-        <div className="Main">
-			
-        </div>
-    );
+  return (
+    <div className={css.Container}>
+      <Input />
+    </div>
+  );
 }
